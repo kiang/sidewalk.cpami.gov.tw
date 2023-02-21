4c9ad12a7bec8df598dbe7f79b737b6ee2bf1558
kiang <kiange@gmail.com>
2023-02-22 02:05:11 +0800

    generate area based json files

--- scripts/02_raw2docs.php
+++ scripts/02_raw2docs.php
@@ -1,54 +1,42 @@
 <?php
 $basePath = dirname(__DIR__);
+
 $jsonPath = $basePath . '/docs/json/sidewalks';
-if (!file_exists($jsonPath)) {
-    mkdir($jsonPath, 0777, true);
-}
 
 $pool = [];
 $fc = [
     'type' => 'FeatureCollection',
     'features' => [],
 ];
-// foreach (glob($basePath . '/raw/DynamicLayer_MapServer_13/*.json') as $jsonFile) {
-//     $json = json_decode(file_get_contents($jsonFile), true);
-
-//     foreach ($json['features'] as $f) {
-//         unset($f['id']);
-//         $city = $f['properties']['COUNTY_NA'];
-//         if (!isset($pool[$city])) {
-//             $pool[$city] = $fc;
-//         }
-//         $f['properties'] = [
-//             'city' => $f['properties']['COUNTY_NA'] . $f['properties']['VILL_NAME'],
-//             'road' => "{$f['properties']['NAME']}在{$f['properties']['PSTART']}與{$f['properties']['PEND']}之間",
-//             'width' => $f['properties']['SW_WTH'],
-//             'length' => $f['properties']['LENGTH'],
-//         ];
-//         $pool[$city]['features'][] = $f;
-//     }
-// }
-
-foreach (glob($basePath . '/raw/DynamicLayer_MapServer_7/*.json') as $jsonFile) {
+foreach (glob($basePath . '/raw/DynamicLayer_MapServer_13/*.json') as $jsonFile) {
     $json = json_decode(file_get_contents($jsonFile), true);
 
     foreach ($json['features'] as $f) {
         unset($f['id']);
         $city = $f['properties']['COUNTY_NA'];
+        $area = $f['properties']['VILL_NAME'];
         if (!isset($pool[$city])) {
-            $pool[$city] = $fc;
+            $pool[$city] = [];
         }
-        print_r($f['properties']); exit();
+        if (!isset($pool[$city][$area])) {
+            $pool[$city][$area] = $fc;
+        }
         $f['properties'] = [
-            'city' => $f['properties']['COUNTY_NA'] . $f['properties']['VILL_NAME'],
             'road' => "{$f['properties']['NAME']}在{$f['properties']['PSTART']}與{$f['properties']['PEND']}之間",
             'width' => $f['properties']['SW_WTH'],
-            'length' => $f['properties']['LENGTH'],
         ];
-        $pool[$city]['features'][] = $f;
+        if (!empty($f['geometry'])) {
+            $pool[$city][$area]['features'][] = $f;
+        }
     }
 }
 
-foreach ($pool as $city => $data) {
-    file_put_contents($jsonPath . '/' . $city . '.json', json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+foreach ($pool as $city => $data1) {
+    $cityPath = $jsonPath . '/' . $city;
+    if (!file_exists($cityPath)) {
+        mkdir($cityPath, 0777, true);
+    }
+    foreach ($data1 as $area => $data) {
+        file_put_contents($cityPath . '/' . $area . '.json', json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    }
 }
